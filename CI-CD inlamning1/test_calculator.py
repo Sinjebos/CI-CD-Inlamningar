d90b821e1045a98ec0fd260ef64b802dd612b26f
Sinjebos <Nicke_95_@hotmail.com>
2021-04-28 14:29:55 +0200

    testing

---
+++ CI-CD inlamning1/test_calculator.py
@@ -0,0 +1,11 @@
+from calculator import Calc
+
+
+class TestCalculator:
+    def test_add(self):
+        assert 4 == Calc.add(2, 2)
+        assert 4 == Calc.add(1, 3)
+
+    def test_sub(self):
+        assert 4 == Calc.sub(6, 2)
+        assert 4 == Calc.sub(8, 4)
